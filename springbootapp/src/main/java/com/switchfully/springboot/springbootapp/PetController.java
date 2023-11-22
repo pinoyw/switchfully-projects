f84812a34df64a145602d75b81a2ac500d59a579
Wietse Pinoy <wietsepinoy@gmail.com>
2023-11-22 16:22:27 +0100

    22/11/23

--- springbootapp/src/main/java/com/switchfully/springboot/springbootapp/PetController.java
+++ springbootapp/src/main/java/com/switchfully/springboot/springbootapp/PetController.java
@@ -2,6 +2,8 @@ package com.switchfully.springboot.springbootapp;
 
 
 import com.switchfully.springboot.springbootapp.pet.PetDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,7 @@ import java.util.List;
 @RequestMapping(value = "pets")
 public class PetController {
     private PetService petService;
-
+    Logger logger = LoggerFactory.getLogger(PetController.class);
     public PetController(PetService petService) {
         this.petService = petService;
     }
@@ -23,6 +25,16 @@ public class PetController {
     @GetMapping(value = "allpets",produces = "application/json")
     public List<PetDTO> getAllPets() {
         return petService.getAllPets();
+    }
+
+    @RequestMapping("log")
+    public String helloExample() {
+        logger.trace("I'll update you on every heartbeat 💓");
+        logger.debug("I'm taking a (sub)step 🚶");
+        logger.info("Business as usual, I processed an action correctly");
+        logger.warn("Warning ⚠ I do not trust this situation?!");
+        logger.error("Help 🆘 Something is broken!");
+        return "<h1>`System.out.println();` is forbidden!</h1><p>Logging for the win!</p>";
     }
 }
 
